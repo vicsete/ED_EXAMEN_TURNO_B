f187f1e829fe48be296fdc1e600d165fccebd815
vicsete <vicsete@gmail.com>
2021-01-13 20:23:18 +0100

    código sin javadoc DNI 24376158E

---
+++ src/analistaed_24376158e/AnalistaED_24376158E.java
@@ -0,0 +1,32 @@
+package analistaed_24376158e;
+
+import java.util.*;
+
+public class AnalistaED_24376158E {
+
+    static final double FACTOR_HORA_EXTRA = 1.2;
+    private String nombreCompleto, dni;
+    private double salarioBasePorHora = 10.56;
+
+    public AnalistaED_24376158E(String nombreCompleto, String dni) {
+        this.nombreCompleto = nombreCompleto;
+        this.dni = dni;
+    }
+
+    public double getSalarioEsteMes(int horasExtra) {
+        double cantidadExtra, salarioFinal;
+        cantidadExtra = horasExtra * this.salarioBasePorHora * FACTOR_HORA_EXTRA;
+        salarioFinal = this.salarioBasePorHora * 40 * 4 + cantidadExtra;
+        salarioFinal = (double) Math.round(salarioFinal * 100d) / 100d;
+        return salarioFinal;
+    }
+
+    // Este método no lo comentaremos con JavaDoc
+    public static void main(String[] args) {
+        // !!!! MODIFICA TU NOMBRE Y TU DNI EN LA SIGUIENTE LÍNEA        
+        AnalistaED_24376158E emp = new AnalistaED_24376158E("VICTOR SENA", "24376158E");
+        int horasExtra = 2;
+        System.out.println("El salario de " + emp.nombreCompleto + " este mes será de " + emp.getSalarioEsteMes(horasExtra) + " euros, al hacer " + horasExtra + " horas extra.");
+    }
+
+}
